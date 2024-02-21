f3adfdf7f9a4d2947b8dca821e9c0db79cd07d6f
nguyenanhung <info@nguyenanhung.com>
2024-02-21 18:46:31 +0700

    Update helper: telegram_simple_message - add markdown

--- helpers/telegram_helper.php
+++ helpers/telegram_helper.php
@@ -9,23 +9,26 @@
  */
 
 if (!function_exists('telegram_simple_message')) {
-    /**
-     * Function telegram_simple_message - Hàm gửi tin nhắn qua Telegram với 1 đoạn text đơn giản
-     *
-     * @param array  $config  Mảng dữ liệu chứa cấu hình config tới Telegram
-     * @param string $chat_id ID của nhóm chat hoặc người đã start chat với BOT
-     * @param string $message Nội dung thông điệp được gửi đi
-     *
-     * @return bool
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 04/01/2021 58:50
-     */
-    function telegram_simple_message($config = array(), $chat_id = '', $message = '')
-    {
-        $telegram = new nguyenanhung\TelegramMessenger\TelegramMessenger();
-        $telegram->setSdkConfig($config)->setChatId($chat_id)->setMessage($message);
-
-        return $telegram->sendMessage();
-    }
+	/**
+	 * Function telegram_simple_message - Hàm gửi tin nhắn qua Telegram với 1 đoạn text đơn giản
+	 *
+	 * @param array $config Mảng dữ liệu chứa cấu hình config tới Telegram
+	 * @param string $chat_id ID của nhóm chat hoặc người đã start chat với BOT
+	 * @param string $message Nội dung thông điệp được gửi đi
+	 * @param bool $markdown Gửi tin sử dụng Markdown
+	 *
+	 * @return bool
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 04/01/2021 58:50
+	 */
+	function telegram_simple_message($config = array(), $chat_id = '', $message = '', $markdown = false)
+	{
+		$telegram = new nguyenanhung\TelegramMessenger\TelegramMessenger();
+		$telegram->setSdkConfig($config)->setChatId($chat_id)->setMessage($message);
+		if ($markdown === true) {
+			$telegram->parseModeMarkdown();
+		}
+		return $telegram->sendMessage();
+	}
 }
