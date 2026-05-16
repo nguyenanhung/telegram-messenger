bee82925387dded39f1992de45be549acd907e3d
nguyenanhung <hungna.dev@gmail.com>
2026-05-17 01:11:11 +0700

    Release v1.1.7

--- src/TelegramMessenger.php
+++ src/TelegramMessenger.php
@@ -22,7 +22,7 @@ class TelegramMessenger
     use TelegramOptions;
 
     const _CLASS_NAME_ = 'TelegramMessenger';
-    const VERSION = '1.1.6';
+    const VERSION = '1.1.7';
     const TELEGRAM_MESSENGER_CONFIG_KEY = 'telegram_messages';
     const TELEGRAM_API = 'https://api.telegram.org/bot';
     const METHOD_GET_ME = '/getMe';
